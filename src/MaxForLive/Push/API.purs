6ae1b11236d5fa0886c50c21fa622b788e8f7f4c
Edsko de Vries <edsko@well-typed.com>
2021-03-05 10:39:20 +0100

    Docs for the Push API

--- src/MaxForLive/Push/API.purs
+++ src/MaxForLive/Push/API.purs
@@ -14,9 +14,23 @@ type Button = { col :: Int, row :: Int }
 type Color  = Int
 
 type Push = {
-      grabButtonMatrix     :: Effect Unit
+      -- | Grab control over the button matrix
+      --
+      -- When Max has control over the button matrix, we can set colors and
+      -- listen to button presses.
+      grabButtonMatrix :: Effect Unit
+
+      -- | Release control over the button matrix
     , releaseButtonMatrix  :: Effect Unit
+
+      -- | Set color of one of the buttons
+      --
+      -- If the Push is not yet initialized, remember the color but don't
+      -- actually change anything on the Push.
     , setButtonMatrixColor :: Button -> Color -> Effect Unit
-    , withButtonMatrixId   :: (Id (Element ButtonMatrix) -> Effect Unit)
-                           -> Effect Unit
+
+      -- | Get the ID of the button matrix (if Push found)
+    , withButtonMatrixId ::
+           (Id (Element ButtonMatrix) -> Effect Unit)
+        -> Effect Unit
     }
